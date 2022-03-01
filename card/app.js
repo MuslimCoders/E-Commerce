8533cf1a2f516f8ea45a7be784cdbd839bed90bd
unknown <nasima.sharipova1967@gmail.com>
2022-03-01 22:02:15 +0500

    finished no responsive

---
+++ card/app.js
@@ -0,0 +1,7 @@
+const sumBtn = document.querySelector(".sum-btn");
+
+const containerModal = document.querySelector(".container-modal");
+
+sumBtn.addEventListener("click", () => {
+    containerModal.style.display = "flex"
+})
